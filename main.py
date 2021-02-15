e15a04cc93f74e3e5a1d4eaa94ba45649b8b4330
Nicolas Padula <nahog@msn.com>
2021-02-15 15:40:34 +0000

    Basic picodisplay emulator

---
+++ main.py
@@ -0,0 +1,38 @@
+from lib import picodisplay as p
+picodisplay = p.PicoDisplay()
+
+buf = bytearray(picodisplay.get_width() * picodisplay.get_height() * 2)
+picodisplay.init(buf)
+
+picodisplay.set_pen(255, 0, 0)
+picodisplay.clear()
+
+picodisplay.set_pen(0, 255, 0)
+picodisplay.pixel(10, 10)
+picodisplay.pixel(10, 11)
+picodisplay.pixel(10, 12)
+picodisplay.pixel(11, 10)
+picodisplay.pixel(11, 11)
+picodisplay.pixel(11, 12)
+picodisplay.pixel(12, 10)
+picodisplay.pixel(12, 11)
+picodisplay.pixel(12, 12)
+
+picodisplay.set_pen(0, 0, 255)
+picodisplay.pixel_span(20, 20, 50)
+picodisplay.pixel_span(80, 100, 500)
+
+pen = picodisplay.create_pen(99, 33, 99)
+picodisplay.set_pen(pen)
+picodisplay.rectangle(100, 60, 30, 60)
+
+picodisplay.set_pen(255, 255, 255)
+picodisplay.circle(200, 40, 10)
+
+picodisplay.update()
+
+picodisplay.set_led(255, 0, 0)
+picodisplay.set_led(0, 255, 0)
+picodisplay.set_led(0, 0, 255)
+
+picodisplay.keep_running()
